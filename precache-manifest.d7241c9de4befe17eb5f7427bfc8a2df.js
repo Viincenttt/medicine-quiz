06f45251c65efd56256e9fe5393459af3bda7d33
Vincent <vincentkok@live.nl>
2020-04-11 09:52:44 +0200

    Updates

---
+++ precache-manifest.d7241c9de4befe17eb5f7427bfc8a2df.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a694d0e8e77a0185982e054c1ee3e224",
+    "url": "/medicine-quiz/index.html"
+  },
+  {
+    "revision": "9b9af105b2353c7138ee",
+    "url": "/medicine-quiz/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "939ecd40833d75306699",
+    "url": "/medicine-quiz/static/js/2.6d7a00b3.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/medicine-quiz/static/js/2.6d7a00b3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9b9af105b2353c7138ee",
+    "url": "/medicine-quiz/static/js/main.711d85ca.chunk.js"
+  },
+  {
+    "revision": "701a8dcd07e58873141d",
+    "url": "/medicine-quiz/static/js/runtime-main.bfd7acfe.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/medicine-quiz/static/media/logo.5d5d9eef.svg"
+  }
+]);
