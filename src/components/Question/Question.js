6a2608dc5e98414d71424eb58c0a0659d5e20929
Vincent <vincentkok@live.nl>
2020-04-11 16:15:05 +0200

    Convert question to functional component

--- src/components/Question/Question.js
+++ src/components/Question/Question.js
@@ -1,36 +1,34 @@
 import React from "react";
 import "./Question.css";
 
-class Question extends React.Component {
-  render() {
-    if (this.props.question === null) {
-      return "";
-    }
-
-    const answerList = this.props.question.allAnswers.map((answer, index) => {
-      return (
-        <li key={this.props.questionNumber + "_" + index}>
-          <input
-            id={this.props.questionNumber + "_" + index}
-            type="checkbox"
-            name="side-effect-answers"
-            value={answer}
-            onChange={this.props.onAnswerClick}
-          />
-          <label htmlFor={this.props.questionNumber + "_" + index}>
-            {answer}
-          </label>
-        </li>
-      );
-    });
-
-    return (
-      <div>
-        <h2>{this.props.question.text}</h2>
-        <ul>{answerList}</ul>
-      </div>
-    );
+function Question (props) {
+  if (props.question === null) {
+    return "";
   }
+
+  const answerList = props.question.allAnswers.map((answer, index) => {
+    return (
+      <li key={props.questionNumber + "_" + index}>
+        <input
+          id={props.questionNumber + "_" + index}
+          type="checkbox"
+          name="side-effect-answers"
+          value={answer}
+          onChange={props.onAnswerClick}
+        />
+        <label htmlFor={props.questionNumber + "_" + index}>
+          {answer}
+        </label>
+      </li>
+    );
+  });
+
+  return (
+    <div>
+      <h2>{props.question.text}</h2>
+      <ul>{answerList}</ul>
+    </div>
+  );
 }
 
 export default Question;
