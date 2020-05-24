00cc72790a799009a1520002d3e0d990ee6200b0
Vincent <vincentkok@live.nl>
2020-05-24 20:30:50 +0200

    Update App component to use Reach hooks

--- src/containers/App.tsx
+++ src/containers/App.tsx
@@ -1,4 +1,5 @@
-import React from 'react';
+import React, { useState, useEffect, useCallback } from 'react';
+
 import Question from '../components/Question/Question';
 import QuestionGenerator from '../services/QuestionGenerator';
 import ResultSummary from '../components/ResultSummary/ResultSummary';
@@ -16,34 +17,33 @@ type AppState = {
   questionNumber: number
 };
 
-class App extends React.Component<AppProps, AppState> {
-  constructor(props: AppProps) {
-    super(props);
-
-    this.state = {
-      ...this.generateStateForNewQuestion(0),
-      questionNumber: 0
-    } as AppState
-  }
-
-  generateStateForNewQuestion = (questionNumber: number): AppState => {
-    return {
-      chosenAnswers: [],
-      hasSubmittedAnswers: false,
-      chosenAnswersAreCorrect: false,
-      question: this.generateNewQuestion(),
-      questionNumber: questionNumber
-    };
-  };
-
-  generateNewQuestion = (): QuestionModel => {
+const App = (props: AppProps) => {
+  const generateNewQuestion = (): QuestionModel => {
     return new QuestionGenerator().generateQuestion();
   }
 
-  onAnswerClick = (e: React.FormEvent<HTMLInputElement>): void => {
+  const [chosenAnswers, setChosenAnswers] = useState<string[]>([]);
+  const [hasSubmittedAnswers, setHasSubmittedAnswers] = useState(false);
+  const [chosenAnswersAreCorrect, setChosenAnswersAreCorrect] = useState(false);
+  const [question, setQuestion] = useState<QuestionModel>(generateNewQuestion());
+  const [questionNumber, setQuestionNumber] = useState(0);  
+
+  const resetStateForNewQuestion = useCallback(() => {
+    setChosenAnswers([]);
+    setHasSubmittedAnswers(false);
+    setChosenAnswersAreCorrect(false);
+    setQuestion(generateNewQuestion());
+    setQuestionNumber((prevState) => prevState + 1);
+  }, [setChosenAnswers, setHasSubmittedAnswers, setChosenAnswersAreCorrect, setQuestion, setQuestionNumber]);
+
+  useEffect(() => {
+    resetStateForNewQuestion();
+  }, [resetStateForNewQuestion]);
+
+  const onAnswerClick = (e: React.FormEvent<HTMLInputElement>): void => {
     const chosenAnswer = e.currentTarget.value;
-    const chosenAnswers = this.state.question.isMultipleChoice ? 
-      [...this.state.chosenAnswers] : 
+    const updatedChosenAnswers = question.isMultipleChoice ? 
+      [...chosenAnswers] : 
       [];
 
     if (e.currentTarget.checked) {
@@ -53,48 +53,44 @@ class App extends React.Component<AppProps, AppState> {
       chosenAnswers.splice(chosenAnswers.indexOf(chosenAnswer), 1);
     }
     
-    this.setState({chosenAnswers: chosenAnswers});
+    setChosenAnswers(updatedChosenAnswers);
   };
 
-  onNextQuestionClick = (): void => {
-    this.setState(this.generateStateForNewQuestion(this.state.questionNumber + 1));
+  const onNextQuestionClick = (): void => {
+    resetStateForNewQuestion();
   };
 
-  onSubmitAnswers = (): void => {
-    let chosenAnswersAreCorrect = this.state.chosenAnswers.length === this.state.question.correctAnswers.length;
-    for (const chosenAnswer of this.state.chosenAnswers) {
-      if (!this.state.question.correctAnswers.includes(chosenAnswer)) {
+  const onSubmitAnswers = (): void => {
+    let chosenAnswersAreCorrect = chosenAnswers.length === question.correctAnswers.length;
+    for (const chosenAnswer of chosenAnswers) {
+      if (!question.correctAnswers.includes(chosenAnswer)) {
         chosenAnswersAreCorrect = false;
       }
     }
 
-    this.setState({ 
-      chosenAnswersAreCorrect: chosenAnswersAreCorrect,
-      hasSubmittedAnswers: true
-    });
+    setChosenAnswersAreCorrect(chosenAnswersAreCorrect);
+    setHasSubmittedAnswers(true);
   };
 
-  render() : JSX.Element {
-    const submitSection = this.state.hasSubmittedAnswers ? 
-      (<ResultSummary 
-          key={'submit_' + this.state.questionNumber}
-          question={this.state.question}
-          chosenAnswersAreCorrect={this.state.chosenAnswersAreCorrect}
-          onNextQuestionClick={this.onNextQuestionClick} />) :
-        <button onClick={this.onSubmitAnswers}>Inleveren</button>
+  const submitSection = hasSubmittedAnswers ? (
+    <ResultSummary 
+      key={'submit_' + questionNumber}
+      question={question}
+      chosenAnswersAreCorrect={chosenAnswersAreCorrect}
+      onNextQuestionClick={onNextQuestionClick} />) :
+    <button onClick={onSubmitAnswers}>Inleveren</button>
 
-    return (
-      <div className="main-content">
-        <Question 
-          key={'question_' + this.state.questionNumber}
-          question={this.state.question}
-          questionNumber={this.state.questionNumber}
-          onAnswerClick={this.onAnswerClick} />
+  return (
+    <div className="main-content">
+      <Question 
+        key={'question_' + questionNumber}
+        question={question}
+        questionNumber={questionNumber}
+        onAnswerClick={onAnswerClick} />
 
-        {submitSection}
-      </div>
-    );
-  }
+      {submitSection}
+    </div>
+  );
 }
 
 export default App;
